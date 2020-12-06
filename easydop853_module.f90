185bf095f4e50e685fc0c24a1b58326d13e68b9a
GasinAn <Gasin185@163.com>
2020-12-07 02:09:19 +0800

    Add files via upload

---
+++ easydop853_module.f90
@@ -0,0 +1,45 @@
+
+    module easydop853_module
+
+    use dop853_module
+    use dop853_constants
+
+    implicit none
+
+    contains
+    
+    subroutine easydop853(fcn,x,xf,y)
+
+    implicit none
+
+    procedure(deriv_func)               :: fcn
+    !! subroutine computing the value of \(dy/dx=f(x,y)\)
+    real(wp),intent(inout)              :: x
+    !! `x` value (input is initial value and output is final value)
+    real(wp),intent(in)                 :: xf
+    !! endpoint of integration (final value of `x`)
+    real(wp),dimension(:),intent(inout) :: y
+    !! `y` value (input is initial value and output is final value)
+
+    integer               :: nmax = 2250000
+    !! maximal number of allowed steps
+    real(wp),dimension(1) :: rtol = 1.0e-12_wp
+    !! relative tolerance
+    real(wp),dimension(1) :: atol = 1.0e-24_wp
+    !! absolute tolerance
+
+    type(dop853_class) :: prop
+    logical :: status_ok
+    integer :: idid
+
+    call prop%initialize(n=size(y),fcn=fcn,nmax=nmax,status_ok=status_ok)
+    if (.not.status_ok) error stop 'initialization error'
+
+    call prop%integrate(x,y,xf,rtol,atol,iout=0,idid=idid)
+    if (idid<0) error stop 'integration failure'
+
+    end subroutine easydop853
+
+    end module easydop853_module
+
+
---
+++ easydop853_test.f90
@@ -0,0 +1,24 @@
+
+    program dop_test
+
+    use dop853_module
+    use dop853_constants
+
+    use easydop853_module
+
+    procedure(deriv_func) :: fcn
+    !! subroutine computing the value of \(dy/dx=f(x,y)\)
+    real(wp)              :: x  = 0.0_wp
+    !! `x` value (input is initial value and output is final value)
+    real(wp)              :: xf = 225000.0_wp
+    !! endpoint of integration (final value of `x`)
+    real(wp),dimension(6) :: y  = [1.0_wp,0.0_wp,0.0_wp,0.0_wp,1.0_wp,0.0_wp]
+    !! `y` value (input is initial value and output is final value)    
+    
+    call easydop853(fcn,x,xf,y)
+    
+    print *, x, y
+
+    end program dop_test
+
+
